f090a97d519535fd0a7c31bac82c5e409ae8e515
scriabinEtude <be_dasan@naver.com>
2020-12-07 00:54:18 +0900

    37:32 not increasing number

--- src/component/Game.js
+++ src/component/Game.js
@@ -1,31 +1,33 @@
 import React, { useState } from 'react'
 import {times} from 'lodash'
 import { MAX_POS } from '../constant'
+import { getInitialTileList } from '../util/tile';
+import useMoveTile from '../hook/useMoveTile';
+import Tile from './Tile'
 
 export default function Game() {
-  const [tileList, setTileList] = useState([]);
+  const [tileList, setTileList] = useState(getInitialTileList);
+  //up down left right
+  useMoveTile(tileList, setTileList);
 
   return (
     <div className="game-container">      
       <div className="grid-container">
-
-        {times(MAX_POS, ()=> {
-          <div className="grid-row">
-          {times(MAX_POS, ()=> {
-            <div className="grid-cell"></div>
-          })}
-        </div>
-        })}
+        {times(MAX_POS, index => (
+          <div key={index} className="grid-row">
+          {times(MAX_POS, index2 => (
+            <div key={index2} className="grid-cell"></div>
+          ))}
+          </div>
+        ))}
 
     </div>
 
 
       <div className="tile-container">
-        {tileList.map(item=>{
-          <div className={`tile tile-${item.value} tile-position-${item.x}-${item.y}`}>
-            <div className="tile-inner">2</div>
-          </div>
-        })}
+        {tileList.map(item=>(
+          <Tile key={item.id} {...item}/>
+        ))}
         
       </div>
     </div>
