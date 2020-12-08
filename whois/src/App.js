74fddc421c0fe0956af0c1d0173fcc9b31e261d6
scriabinEtude <be_dasan@naver.com>
2020-12-08 23:20:21 +0900

    Lec.43 찾아야한다

---
+++ whois/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Search from './search/container/Search'
+import 'antd/dist/antd.css'
+import './font.css'
+
+
+export default function App() {
+  return <Search />
+}
