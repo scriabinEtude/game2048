43f1dab7e6c1a453d65f0cdd9babdf45ffd308c3
lim_hangyoel <lim_hangyoel@radcns.com>
2020-12-09 16:51:29 +0900

    Lec.48

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React, { useEffect, useState } from 'react'
-import AboveGame from './component/AboveGame'
-import Game from './component/Game'
-import Header from './component/Header'
-import useLocalStorage from './hook/useLocalStorage'
+import {createStore, applyMiddleware } from 'redux'
 
-
-export default function App() {
-  const [score, setScore] = useState(0)
-  const [bestScore, setBestScore] = useLocalStorage('bestScore', 0)
- 
-  useEffect(() => {
-    if(score > bestScore){
-      setBestScore(score);
-    }
-  }, [])
-
-  return (
-    <div className="container">
-      <Header score={score} bestScore={0} />
-      <AboveGame />
-      <Game setScore={setScore} />
-    </div>
-  );
+const middleware1 = store => next => action => {
+  const result = next(action)
+  return result
 }
 
+const middelware2 = store => next => action => {
+  const result = next(action)
+}
+
+const myReducer = (state, action) => {
+  return state;
+}
+
+const store = createStore(myReducer, applyMiddleware(middleware1, middelware2));
--- whois/src/search/container/Search.js
+++ whois/src/search/container/Search.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Row, Col, Typography } from 'antd'
+import { Row, Col } from 'antd'
 import Settings from '../component/Settings'
+import SearchInput from './SearchInput'
 
 export default function Search() {
   return <>
@@ -9,11 +10,11 @@ export default function Search() {
     </Row>
     <Row justify="center" style={{marginTop:100}}>
         <Col>
-            <Typography.Title style={{fontFamily:'Nanum Pen Script'}}>찾 아 야 한 다</Typography.Title>
+            <p style={{fontSize:'60px', fontFamily:'Nanum Pen Script'}}>찾 아 야 한 다</p>
         </Col>
     </Row>
     <Row justify="center" style={{marginTop:50}}>
-        <Col span={12}>검색</Col>
+        <Col span={12}><SearchInput /></Col>
     </Row>
   </>
-}
+} 
